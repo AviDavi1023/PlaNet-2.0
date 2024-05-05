f135d197c0b285808ef8ea9e39ec30f9f7da0992
AviDavi1023 <avidavi1023@gmail.com>
2024-05-05 09:16:39 -0700

    user_navbar_changes

--- PlantID/apps.py
+++ PlantID/apps.py
@@ -4,3 +4,7 @@ from django.apps import AppConfig
 class PlantidConfig(AppConfig):
     default_auto_field = 'django.db.models.BigAutoField'
     name = 'PlantID'
+
+class UserAuthConfig(AppConfig):
+   default_auto_field = 'django.db.models.BigAutoField'
+   name = 'user_auth'
--- PlantID/views.py
+++ PlantID/views.py
@@ -1,8 +1,8 @@
-from django.shortcuts import render
-from django.http import HttpResponse
+from django.shortcuts import render, redirect
+from django.contrib.auth import authenticate, login, logout
+from .forms import SignUpForm
 
 def home(request):
-    # return HttpResponse("Hello, world!")
     return render(request, 'home.html', {})
 
 def about(request):
@@ -13,3 +13,40 @@ def upload(request):
 
 def howtouse(request):
     return render(request, 'howtouse.html', {})
+
+def login_user(request):
+   if request.user.is_authenticated:
+       return redirect('home')
+  
+   if request.method == "POST":
+       username = request.POST['username']
+       password = request.POST['password']
+       user = authenticate(request, username=username, password=password)
+       if user is not None:
+           login(request, user)
+           return redirect('home')
+       else:
+           # need an error message
+           return redirect('login')
+   else:
+       return render(request, 'login.html', {})
+
+def logout_user(request):
+   logout(request)
+   return redirect('home')
+
+def register_user(request):
+   if request.method == "POST":
+       form = SignUpForm(request.POST)
+       if form.is_valid():
+           form.save()
+           username = form.cleaned_data['username']
+           password = form.cleaned_data['password1']
+           user = authenticate(username=username, password=password)
+           login(request, user)
+           return redirect('home')
+      
+   else:
+       form = SignUpForm
+
+   return render(request, 'register.html', {"form": form})
