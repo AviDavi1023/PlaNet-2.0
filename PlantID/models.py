f3f3ba164ef036c3055ebee00b692a54f4f293aa
AviDavi1023 <avidavi1023@gmail.com>
2024-05-12 09:14:49 -0700

    added models

--- PlantID/models.py
+++ PlantID/models.py
@@ -1,3 +1,12 @@
 from django.db import models
+from django.contrib.auth.models import User
 
-# Create your models here.
+class Plant (models.Model):
+    user = models.ForeignKey(User, related_name='plant', on_delete=models.CASCADE)
+    name = models.CharField(blank=True, null=True, )
+    date_found = models.DateTimeField(auto_now_add=True)
+    location = models.CharField(blank=True, null=True, )
+    comment = description = models.CharField()
+
+    def __str__(self):
+        return self.name
