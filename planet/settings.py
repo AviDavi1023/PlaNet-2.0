6e6ae337ea8654719b942f4e7d78e3d4b6a717a4
AviDavi1023 <avidavi1023@gmail.com>
2024-06-16 08:32:16 -0700

    6/16/24

--- planet/settings.py
+++ planet/settings.py
@@ -119,7 +119,8 @@ USE_TZ = True
 
 STATIC_URL = 'static/'
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
-# Default primary key field type
-# https://docs.djangoproject.com/en/5.0/ref/settings/#default-auto-field
+
+MEDIA_URL = 'media/'
+MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
